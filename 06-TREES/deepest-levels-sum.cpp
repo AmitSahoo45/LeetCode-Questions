649af3688ac6b92bf9e4bcfc76a1648b82d5afeb
miamitsahoo@gmail.com <miamitsahoo@gmail.com>
2022-03-30 20:42:01 +0530

    Added More code

--- 06-TREES/deepest-levels-sum.cpp
+++ 06-TREES/deepest-levels-sum.cpp
@@ -14,9 +14,54 @@ struct TreeNode
 class Solution
 {
 public:
-    int deepestLeavesSum(TreeNode *root)
+    int deepestLeavesSum(TreeNode *root, int sum = 0)
     {
-        
+        if (root == nullptr)
+            return 0;
+
+        queue<TreeNode *> q;
+        q.push(root);
+
+        while (!q.empty())
+        {
+            sum = 0;
+            int size = q.size();
+            for (int i = 0; i < size; i++)
+            {
+                TreeNode *node = q.front();
+                q.pop();
+                sum += node->val;
+                if (node->left)
+                    q.push(node->left);
+                if (node->right)
+                    q.push(node->right);
+            }
+        }
+        return sum;
+    }
+};
+// Solution - 1
+// Runtime: 134 ms, faster than 54.79% of C++ online submissions for Deepest Leaves Sum.
+// Memory Usage: 61.7 MB, less than 43.81% of C++ online submissions for Deepest Leaves Sum.
+// Time Complexity : O(N)
+// Space Complexity : O(N)
+
+class Solution2
+{
+public:
+    int deepest = 0, sum = 0;
+    int deepestLeavesSum(TreeNode *root, int depth = 0)
+    {
+        if (!root)
+            return 0;
+        if (!root->left && !root->right)
+            if (depth == deepest)
+                sum += root->val;
+            else if (depth > deepest)
+                sum = root->val, deepest = depth;
+        deepestLeavesSum(root->left, depth + 1);
+        deepestLeavesSum(root->right, depth + 1);
+        return sum;
     }
 };
 
