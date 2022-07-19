71171becb9428a63637495fbc5a4fb2a8a999125
miamitsahoo@gmail.com <miamitsahoo@gmail.com>
2022-07-19 22:41:00 +0530

    added code

--- 03-STACK/MaximumTwinSumofaLinkedList.cpp
+++ 03-STACK/MaximumTwinSumofaLinkedList.cpp
@@ -10,13 +10,35 @@ struct ListNode
     ListNode(int x, ListNode *next) : val(x), next(next) {}
 };
 
+// My solution ---->>>
+// using array
 class Solution
 {
 public:
+    vector<int> v;
     int pairSum(ListNode *head)
     {
+        while (head)
+        {
+            v.push_back(head->val);
+            head = head->next;
+        }
+        int n = v.size(), maxi = INT_MIN;
+        for (int i = 0; i < n / 2; i++)
+            maxi = max(maxi, (v[i] + v[n - 1 - i]));
+
+        return maxi;
     }
 };
+
+// using stack
+class StackSolution
+{
+    public:
+    
+};
+
+// Other Solutions --->>>
 
 int main()
 {
