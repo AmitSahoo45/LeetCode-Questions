227dbcffffa0522255bd998e6baec97eed991dab
miamitsahoo@gmail.com <miamitsahoo@gmail.com>
2022-07-31 23:41:52 +0530

    Added more code

--- 04-QUEUE/RevealCardsInIncreasingOrder.cpp
+++ 04-QUEUE/RevealCardsInIncreasingOrder.cpp
@@ -6,21 +6,7 @@ class Solution
 public:
     vector<int> deckRevealedIncreasing(vector<int> &deck)
     {
-        vector<int> ans(deck.size());
-        int n = deck.size();
-        sort(deck.begin(), deck.end());
-        queue<int> q;
-        for (int i = 0; i < n; i++)
-            q.push(i);
-
-        for (int i = 0; i < n; i++)
-        {
-            ans[q.front()] = deck[i];
-            q.pop();
-            q.push(q.front());
-            q.pop();
-        }
-        return ans;
+        
     }
 };
 
